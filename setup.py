8db7e510c5c6594ee6686d4fe95813c4e0850a25
satyareddy-d <dsreddy.py@gmail.com>
2020-09-02 19:39:05 +0530

    added dependencies to setup.py file

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
-
+requirements = [
+    'celery-4.4.7',
+    'requests-2.24.0'
+]
 # need to hanlde other paramers of setup() method
 setup(
     name="fes",
     version="1.0",
     packages=find_packages(),
+    install_requires=requirements
 )
